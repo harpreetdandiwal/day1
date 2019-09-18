2a083b590991c80f0ccdf07d672bfafddc21fa32
macstudent <dandiwal357>
2019-09-18 19:25:07 -0400

    one command deleted

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -5,10 +5,14 @@ public class FirstDay
     public static void main(String[] args)
     {
 System.out.println("welcome to java programming");
+//variable declaration
 int a,b,c;
+//value assignment
 a=10;
 b=15 ;
+//perform add
 c=a+b;
+
 System.out.println("sum is" +c);
     }
 }
