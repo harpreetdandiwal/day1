acc1228c3b5b206b8693dc5132c0f9859db3a24e
MacStudent <macstudent@126-iMac17.local>
2019-09-18 19:16:52 -0400

    Initial commit

---
+++ src/com/lambton/FirstDay.java
@@ -0,0 +1,14 @@
+package com.lambton;
+
+public class FirstDay
+{
+    public static void main(String[] args)
+    {
+System.out.println("welcome to java programming");
+int a,b,c;
+a=10;
+b=15 ;
+c=a+b;
+System.out.println("sum is" +c);
+    }
+}
