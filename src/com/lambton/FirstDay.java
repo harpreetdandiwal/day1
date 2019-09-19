a0f0e26ecbe54659b853794cc138948171ec1bd9
macstudent <dandiwal357>
2019-09-18 20:47:53 -0400

    name declaration

--- src/com/lambton/FirstDay.java
+++ src/com/lambton/FirstDay.java
@@ -1,5 +1,8 @@
 package com.lambton;
 
+import sun.jvm.hotspot.types.CIntegerField;
+import sun.jvm.hotspot.types.CIntegerType;
+
 public class FirstDay
 {
     public static void main(String[] args)
@@ -10,9 +13,30 @@ int a,b,c;
 //value assignment
 a=10;
 b=130;
-//perform multiplication
-c=a*b;
 
-System.out.println("multiplication is " +c);
+//perform multiplication
+c=a+b;
+
+System.out.println("Sum is " + c);
+int x = Integer.MAX_VALUE;
+System.out.println("x  " + x);
+
+int y= x +1;
+System.out.println("y" +1);
+System.out.println("min 1" + Integer.MAX_VALUE);
+
+student s1 =new student();
+        s1.setvalues();
+s1.display();
+student s2 = new student();
+s2.studentID=2;
+s2.student_name="anmol sharma";
+s2.display();
+student s3=new student();
+s3.studentID=3;
+s3.student_name="rizul goyal";
+
+
+s3.display();
     }
 }
--- src/com/lambton/student.java
+++ src/com/lambton/student.java
@@ -4,4 +4,15 @@ public class student
 {
     int studentID;
     String student_name;
+    void  setvalues()
+    {
+        studentID =1;
+        student_name ="harpreet";
+            }
+
+void display()
+{
+    System.out.println("Student id :" + studentID);
+    System.out.println("student name : " + student_name);
+}
 }
